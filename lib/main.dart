cdf8361b562a89e2b1d08c7e6aabd189ea6636f5
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-13 17:18:21 +0530

    Just for sake I message

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
+    return Container();
   }
 }
