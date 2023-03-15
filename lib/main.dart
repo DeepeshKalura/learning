c9f86855844faa10369291c5d3aee2a5f9238e2c
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-15 08:16:54 +0530

    I have make a rough screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+  final double value = 0.0;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo Home Page'),
+      ),
+      body: Column(
+        children: [
+          const Center(
+            child: Text('Hello World'),
+          ),
+          Slider(value: value, onChanged: (val) {}),
+          Expanded(
+            child: Container(
+              color: Colors.red,
+              child: const Text('Container 1'),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+              child: const Text('Container 2'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
